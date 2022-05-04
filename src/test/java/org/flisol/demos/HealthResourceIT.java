8a513285ea85d5c810a5c0618051c80078b398e6
Jorge Francisco Varela Gutiérrez <jorge.fco.varela@gmail.com>
2022-05-03 22:16:01 -0500

    Initial COmmit

---
+++ src/test/java/org/flisol/demos/HealthResourceIT.java
@@ -0,0 +1,9 @@
+package org.flisol.demos;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class HealthResourceIT extends HealthResourceTest {
+
+    // Execute the same tests but in native mode.
+}
